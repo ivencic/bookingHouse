db97955ce958d7371712c9e068a44a447e9d09e6
ivencic <ivencic@gmail.com>
2024-07-14 14:26:06 +0200

    first commit

---
+++ announces/permisions/set_tokens.py
@@ -0,0 +1,21 @@
+"""import datetime
+
+from django.utils.deprecation import MiddlewareMixin
+from rest_framework.request import Request
+from rest_framework_simplejwt.exceptions import TokenError
+from rest_framework_simplejwt.tokens import AccessToken
+
+
+class JWTAuthenticationsMiddleware(MiddlewareMixin):
+
+    def process_request(self, request: Request, **kwargs):
+        access_token = request.COOKIES.get('access_token')
+        refresh_token = request.COOKIES.get('refresh_token')
+
+        if access_token:
+            try:
+                token = AccessToken(access_token)
+                if datetime.fromtimestamp(token['exp']) < datetime.now():
+                    raise TokenError('Token is expired')
+                request.META['HTTP_AUTHORIZATION'] = f'JWT {access_token}'
+"""
